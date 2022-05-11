f04274602a843dc637e57929362f4e3f6bb2d382
MariaDuda <mariabevenuto062@gmail.com>
2022-05-11 13:42:15 -0300

    using map for show the questions on react

--- zapprecall/src/pages/perguntas/index.js
+++ zapprecall/src/pages/perguntas/index.js
@@ -1,3 +1,5 @@
+
+import {  useState } from "react";
 import "./style.css";
 const logoZapPequena = "assets/logo-pequeno.png"
 const seta = "assets/perguntaSeta.png"
@@ -5,37 +7,56 @@ const seta = "assets/perguntaSeta.png"
 
 const perguntasObj = [
     {
-        question: "pergunta 1"
+        question: "pergunta 1",
+        react:  " o React é_"
     },
     {
-        question: "pergunta 2"
+        question: "pergunta 2",
+        react: "O que é JSX? "
     },
     {
-        question: "pergunta 3"
+        question: "pergunta 3",
+        react : "componentes devem inicar com__"
     },
     {
-        question: "pergunta 4"
+        question: "pergunta 4",
+        react:"podemos colocar __ dentro do JSX"
     },
     {
-        question: "pergunta 5"
+        question: "pergunta 5",
+        react: " o ReactDom nos ajuda__"
     },
     {
-        question: "pergunta 6"
+        question: "pergunta 6",
+        react:"usamos npm para__"
     },
     {
-        question: "pergunta 7"
+        question: "pergunta 7",
+        react:"usamos props para__ "
     },
     {
-        question: "pergunta 8"
+        question: "pergunta 8",
+        react:"usamos estad0 (state) para __"
     }
 ]
-function Pergunta({question , index}){
+function Pergunta({question , index , react}){
+    const [card , setCard] = useState(true)
+    if(card === true){
     return(
         <div key={index} className="pergunta">
             <h1 className="fonte">{question}</h1>
-            <img className="diminuir" src={seta} alt="seta de pergunt" />
+            <img onClick={() => setCard(false)} className="diminuir" src={seta} alt="seta de pergunt" />
         </div>
     )
+    }
+    if(card === false){
+        return (
+            <div key={index} className="pergunta">
+            <h1 className="fonte">{react}</h1>
+            <img onClick={() => setCard(false)} className="diminuir" src={seta} alt="seta de pergunt" />
+        </div>
+        )
+    }
 }
 export default function Perguntas() {
     return (
@@ -49,9 +70,11 @@ export default function Perguntas() {
                   <Pergunta
                  question = {perg.question}
                  index = {perg.index}
+                 react = {perg.react}
                  />
               )
           })}
+          <footer>0/8 CONCLUÍDOS</footer>
         </div>
     )
 }
